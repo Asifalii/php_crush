09834d00cc16d212134e90c677805174b9f40144
Asifalii <aliasif1473695@gmail.com>
2022-03-26 00:00:25 +0600

    multidimensional array with example

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+ $multi_dimensional_array=[
+    "asif"=> [
+        "roll"=>1,
+        "age"=>12,
+        "sex"=>"male",
+
+    ],
+    "yessha"=>[
+        "roll"=>1,
+        "age"=>12,
+        "sex"=>"male",
+    ]
+ ];
+echo "<pre>";
+print_r($multi_dimensional_array);
+echo "</pre>";
+
+//  foreach($multi_dimensional_array as $key=>$value){
+//     echo $key ;
+
+
+//     echo "<br>";
+//     foreach($value as $new){
+//         echo $new . " ";
+//     }
+//     echo "<br>";
+//  } 
+   
+?>
