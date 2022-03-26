5664310a9c832f174734f88e5cdbcfa9b5ad348c
Asifalii <aliasif1473695@gmail.com>
2022-03-26 11:42:41 +0600

    mutidimension 2

---
+++ multidimension_array.php
@@ -0,0 +1,11 @@
+<?php 
+    $information=array(
+        array("volbo",22,14),
+        array("bmw",2,4),
+        array("mvd ",55,44),
+    );
+
+    echo $information[0][0]." "."In stock"." ".$information[0][1]." " ."Sold".$information[0][2]."<br>";
+    echo $information[1][0].$information[1][1].$information[1][2]."<br>";
+    echo $information[2][0].$information[2][1].$information[2][2]."<br>";
+?>
